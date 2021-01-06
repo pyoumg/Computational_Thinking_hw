1029f8aa30d03fcbbaac450fd4577cc2fe5d68b1
pyoumg (seyoung) <50650579+pyoumg@users.noreply.github.com>
2021-01-06 17:18:52 +0900

    Add files via upload

---
+++ s7/7-1.py
@@ -0,0 +1,6 @@
+weekdays = ['월','화','수','목','금','토','일']
+month,day=input("날짜를 입력하시오(예:10/1) : ").split('/')
+#10월 중 하루니까 day만 필요함
+day=int(day)
+month=int(month)
+print("{}월 {}일은 {}요일이다".format(month,day,weekdays[(day%7+2)%7]))
